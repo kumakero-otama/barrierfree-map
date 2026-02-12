eadab02c0774582f89ffe148f20c7198ec617d56
kumakero-otama <kumakero.otama@gmail.com>
2026-02-12 19:34:49 +0900

    v1.10.2 ・道情報投稿画面にもバージョン表記を入れた ・道情報投稿画面の写真ライブラリボタンを押すと、スマホの写真ライブラリから画像を選択して、アプリ画面にそれを表示できるようにした

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "1.10.1"; // このバージョンはpackage.jsonから自動生成されます
+const CACHE_VERSION = "1.10.2"; // このバージョンはpackage.jsonから自動生成されます
 const CACHE_NAME = `barrierfree-map-v${CACHE_VERSION}-${Date.now()}`;
 const CORE_ASSETS = [
   "/",
