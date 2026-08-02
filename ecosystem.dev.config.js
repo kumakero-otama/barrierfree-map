139ba1e9a2550e1386b61267def2f7dc596cff91
kumakero-otama <kumakero.otama@gmail.com>
2026-08-02 15:38:36 +0900

    開発UI10のCORSを許可

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -13,7 +13,7 @@ module.exports = {
         HTTP_PORT: "3100",
         HTTPS_PORT: "3101",
         DB_CONFIG_PATH: `${__dirname}/config.dev.yaml`,
-        CORS_ALLOWED_ORIGINS: "http://localhost:8000,http://127.0.0.1:8000",
+        CORS_ALLOWED_ORIGINS: "http://localhost:8000,http://127.0.0.1:8000,https://barrierfree-map.tail5de5e1.ts.net:10001",
       },
     },
   ],
